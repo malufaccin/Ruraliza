84e02df7075f196e4bfe18ddb29443b4253d8ed0
Maria Luiza Duarte Faccin <20211IMI030@cefetes.br>
2024-07-04 08:18:42 -0300

    Ruraliza

---
+++ ProjetoBase/routes/cliente_routes.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+
+from util.templates import obter_jinja_templates
+
+router = APIRouter()
+templates = obter_jinja_templates("templates/cliente")
+
+
+@router.get("/", response_class=HTMLResponse)
+async def get_root(request: Request):
+    return templates.TemplateResponse("pages/index.html", {"request": request})
+
