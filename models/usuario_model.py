6f61e636b67a1a81608105f98e16f20124826c5b
malufaccin <145558786+malufaccin@users.noreply.github.com>
2024-10-04 07:40:40 -0300

    atualização do usuario model

--- models/usuario_model.py
+++ models/usuario_model.py
@@ -7,6 +7,16 @@ class Usuario:
     id: Optional[str] = None
     nome: Optional[str] = None
     email: Optional[str] = None
+    cpf: Optional[int] = None
+    data_nascimento: Optional[int] = None
     senha: Optional[str] = None
+    cpr: Optional[int] = None
+    endereco: Optional[str] = None
+    numero: Optional[int] = None
+    cnpj: Optional[int] = None
+    tipo_veiculo: Optional[str] = None
+    cor: Optional[str] = None
+    placa: Optional[str] = None
+    cnh: Optional[int] = None
     perfil: Optional[int] = None
     token: Optional[str] = None
